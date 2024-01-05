2d6043a9a384dd9034a429faecffe0fe764687e6
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-01-05 17:12:31 +0300

    feat[JDA-0102]: add themes css file with basic themes colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import style from './page.module.scss';
+import classNames from 'classnames';
+import '../styles/themes.css';
+
+const cx = classNames.bind(style);
 
 export default function Home() {
-	return <main>Hello World</main>;
+	return (
+		<main>
+			<p className={style.title}>Hello World</p>
+		</main>
+	);
 }
