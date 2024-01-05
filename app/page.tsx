1a0a573457ac527d53d7560ec52c75280eb72e7b
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-01-06 00:04:47 +0300

    feat[JDA-0203]: add footer layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import style from './page.module.scss';
 import classNames from 'classnames/bind';
 import '../styles/themes.css';
 import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 
 const cx = classNames.bind(style);
 
 export default function Home() {
 	return (
-		<main>
+		<main className={cx('main')}>
 			<Header />
+			<Footer />
 		</main>
 	);
 }
