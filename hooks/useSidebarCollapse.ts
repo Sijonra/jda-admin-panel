093ef3a1e7498b642b3ee033c51e099d1043c68f
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2024-01-07 18:12:20 +0300

    feat[JDA-0304]: add collpse function to navBar

--- hooks/useSidebarCollapse.ts
+++ hooks/useSidebarCollapse.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand';
 
-interface IScrollBarCollapse {
+interface ISideBarCollapse {
 	isOpen: boolean;
 	toggleScrollBarCollapse: () => void;
 }
 
-const useScrollBarCollapse = create<IScrollBarCollapse>((set) => ({
+const useSideBarCollapse = create<ISideBarCollapse>((set) => ({
 	isOpen: true,
 	toggleScrollBarCollapse: () => set((state) => ({ isOpen: !state.isOpen })),
 }));
 
-export default useScrollBarCollapse;
+export default useSideBarCollapse;
